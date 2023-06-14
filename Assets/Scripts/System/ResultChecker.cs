add731f5aa3bb148eb9319bc160c157dcddc5d10
Ray <rei.piyo.55687@gmail.com>
2023-06-14 19:51:06 +0900

    [add]正誤判定(途中)

--- Assets/Scripts/System/ResultChecker.cs
+++ Assets/Scripts/System/ResultChecker.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UniRx;
 public class ResultChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PlayerValues _pv = default;
     void Start()
     {
-        
+        _pv = PlayerValues.Instance;
+        //PlayerConditionの変更通知を購読
+        _pv.OnBitFlagVariableChanged.Subscribe(newValue =>
+        {
+            if (newValue.HasFlag(PlayerValues.PlayerCondition.AnswerCheck))
+            {
+                //正誤判定してスコア加算する
+            }
+
+            //PlayerValuesはシングルトンだが、念のためDispose（購読解除）しておく
+        }).AddTo(_pv);
+    
     }
 
     // Update is called once per frame
